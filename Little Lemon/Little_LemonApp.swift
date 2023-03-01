046a98517f77604cdb81e630afe718d33c1d291c
Arslan Mushtaq <arslanark@gmail.com>
2023-03-01 08:33:18 +0500

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Arslan Mushtaq on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
